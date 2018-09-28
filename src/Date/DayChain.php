abb719a3dd058b44c55d00eb0541ff74dd9c7b88
Jon Gotlin <jon@jon.se>
2018-09-28 20:05:12 +0200

    Added classes to extend swedish parent

---
+++ src/Date/DayChain.php
@@ -0,0 +1,8 @@
+<?php declare (strict_types=1);
+
+namespace JGI\DanishDates\Date;
+
+class DayChain extends \JGI\SwedishDates\Date\DayChain
+{
+
+}
---
+++ src/Manager/DateManager.php
@@ -0,0 +1,8 @@
+<?php declare(strict_types=1);
+
+namespace JGI\DanishDates\Manager;
+
+class DateManager extends \JGI\SwedishDates\Manager\DateManager
+{
+
+}
